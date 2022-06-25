43a1f953078c2779b6f8d29273f8f3346860bb35
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2022-06-25 13:46:29 -0500

    Downed / Dead / Money Checks + Optimization

--- randol_grandma/server/sv_grandma.lua
+++ randol_grandma/server/sv_grandma.lua
@@ -1,11 +1,24 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local moneytype = 'crypto' -- 'cash' 'bank' or 'crypto'
+local payment = 50 -- Payment amount
 
 
--- Charge the player from bank.
-
-RegisterServerEvent('randol_grandma:server:grandmafee')
-AddEventHandler('randol_grandma:server:grandmafee', function()
+RegisterServerEvent('randol_grandma:server:checkfunds', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.RemoveMoney('bank', 2000) --$2000 is the current price, change it to whatever.
+    local balance = Player.Functions.GetMoney(moneytype)
+
+    if balance >= payment then -- Checks to make sure player has enough funds
+        TriggerClientEvent('randol_grandma:reviveplayer', source) -- Starts Progressbar for Reviving
+    else
+        TriggerClientEvent('QBCore:Notify', source, "You don't have enough crypto to pay Grandma!", "error")
+    end
+
+end)
+
+-- Charges Player
+RegisterServerEvent('randol_grandma:server:grandmafee', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    Player.Functions.RemoveMoney(moneytype, payment) -- Removes money type and amount
 end)
