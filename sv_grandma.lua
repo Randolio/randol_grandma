f3e6ea209875228560b8521e45d37b481bbcb3a7
Randolio <103154494+Randolio@users.noreply.github.com>
2024-02-20 20:11:38 +0000

    feat(server): comment for moneyType.

--- sv_grandma.lua
+++ sv_grandma.lua
@@ -5,7 +5,7 @@ Server = {
         [2] = {coords = vec4(1974.38, 3820.33, 33.43, 215.01), model = 'cs_nigel', name = 'Grandpa', busy = false},
     },
     cost = 500,
-    moneyType = 'bank', -- cash/bank
+    moneyType = 'bank', -- cash/bank ('cash' will convert to 'money' for ESX, so keep it as 'cash' if you wanna use cash and let the bridge handle it)
     duration = 10000,
 }
 
