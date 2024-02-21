d3ccf58fb0dc0ce5fb839b479dc12223bec40ddd
Randolio <103154494+Randolio@users.noreply.github.com>
2024-02-21 05:57:56 +0000

    FUCK ZOO

--- sv_grandma.lua
+++ sv_grandma.lua
@@ -1,8 +1,8 @@
-Server = {
+local Server = {
     checkDead = true,
     locations = { -- Multi location support.
-        [1] = {coords = vec4(2432.59, 4966.1, 46.81, 51.92), model = 'ig_mrs_thornhill', name = 'Grandma', busy = false},
-        [2] = {coords = vec4(1974.38, 3820.33, 33.43, 215.01), model = 'cs_nigel', name = 'Grandpa', busy = false},
+        {coords = vec4(2432.59, 4966.1, 46.81, 51.92), model = 'ig_mrs_thornhill', name = 'Grandma', busy = false},
+        {coords = vec4(1974.38, 3820.33, 33.43, 215.01), model = 'cs_nigel', name = 'Grandpa', busy = false},
     },
     cost = 500,
     moneyType = 'bank', -- cash/bank ('cash' will convert to 'money' for ESX, so keep it as 'cash' if you wanna use cash and let the bridge handle it)
